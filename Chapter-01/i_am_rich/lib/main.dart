87381b7a8872e6ecf4533bec175bbc35c85c2366
izimartin <izimartinz@gmail.com>
2024-01-31 03:55:47 +0100

    First Project Completed

---
+++ Chapter-01/i_am_rich/lib/main.dart
@@ -0,0 +1,24 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          centerTitle: true,
+          titleTextStyle: TextStyle(color: Colors.white, fontSize: 20),
+          title: Text('I Am Rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
