f8ad7b668a9960bf2561a8a9202070878f563ced
izimartin <izimartinz@gmail.com>
2024-03-09 19:09:47 +0100

    XylophoneApp

--- xylophone_flutter/lib/main.dart
+++ xylophone_flutter/lib/main.dart
@@ -1,14 +1,52 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/widgets.dart';
 
-void main() => runApp(XylophoneApp());
+void main() => runApp(const XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  void playSound(int soundNumber) {
+    AudioPlayer p = AudioPlayer();
+    p.play(
+      AssetSource('note$soundNumber.wav'),
+    );
+  }
+
+  Expanded buildKey(int soundNumber, Color color) {
+    return Expanded(
+      child: TextButton(
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+        ),
+        child: const Text(''),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(.playSound(1), Colors.red),
+              buildKey(.playSound(2), Colors.orange),
+              buildKey(.playSound(3), Colors.yellow),
+              buildKey(.playSound(4), Colors.green),
+              buildKey(.playSound(5), Colors.teal),
+              buildKey(.playSound(6), Colors.blue),
+              buildKey(.playSound(7), Colors.purple),
+            ],
+          ),
         ),
       ),
     );
