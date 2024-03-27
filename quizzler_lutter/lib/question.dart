8ab4dcf443e2d1840ecc1349c2d4828fb39b051d
izimartin <izimartinz@gmail.com>
2024-03-27 01:55:36 +0100

    Adding a Constructor

---
+++ quizzler_lutter/lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+  String questionText;
+  bool questionAnswer;
+
+  Question({String q = '', bool a = false})
+      : questionText = q,
+        questionAnswer = a {
+    questionText = q;
+    questionAnswer = a;
+  }
+}
